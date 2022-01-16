b619813cb8b3c131f13f59b99ccbf4a3fe774aa4
gdssvpp <pablonogueira16@hotmail.com>
2022-01-16 20:42:33 -0300

    boas praticas

---
+++ javazada/src/variaveiseafins/variaveiseafins.java
@@ -0,0 +1,54 @@
+package variaveiseafins;
+
+/**
+ * Classe de exemplo para o Exercício de Variáveis, Tipos de Dados e Operadores Aritméticos.
+ */
+public class variaveiseafins {
+
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn24678_md = 10;
+        //int asrn2@46%78_md = 10;
+
+        asrn24678md = 100;
+        //asrn24678_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn24678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+    //basicamente são as boas praticas
+    //do que pode ou não usar nas variáveis
+    //deu pra entender legal
+}
