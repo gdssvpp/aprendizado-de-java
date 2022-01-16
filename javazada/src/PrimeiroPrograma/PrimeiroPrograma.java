5a4ad674a556d7a4a724cbc93d2bdbeaaf5f1b4d
gdssvpp <pablonogueira16@hotmail.com>
2022-01-16 16:43:04 -0300

    Primeiro commit

---
+++ javazada/src/PrimeiroPrograma/PrimeiroPrograma.java
@@ -0,0 +1,36 @@
+package PrimeiroPrograma;
+
+import PrimeiroPrograma.model.Livro;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("O Java do Javali", 142);
+        System.out.println(livro1);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas){
+        this.nome = nome;
+        this.numPaginas;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+}
---
+++ javazada/src/calculadorajava/calculadorajava.java
@@ -0,0 +1,41 @@
+package src.calculadorajava;
+
+public class calculadorajava {
+}
+import java.util.Scanner;
+
+public Calculadora() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um valor: ");
+        int a = scan.nextInt();
+        System.out.println("Digite outro valor: ");
+        int b = scan.nextInt();
+        int somar = somar(a, b);
+        int subtratir = subtratir(a, b);
+        int multiplicar = multiplicar(a, b);
+        float dividir = dividir((float)a, (float)b);
+        System.out.println("Soma: " + somar);
+        System.out.println("Subtração: " + subtratir);
+        System.out.println("Divisão: " + dividir);
+        System.out.println("Multiplicar: " + multiplicar);
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtratir(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static float dividir(float a, float b) {
+        return a / b;
+    }
+}
