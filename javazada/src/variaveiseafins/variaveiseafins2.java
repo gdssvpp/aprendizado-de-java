585a95afb90965b9f38f6101d34a302feea07754
gdssvpp <pablonogueira16@hotmail.com>
2022-01-17 20:49:17 -0300

    tipos de dados

---
+++ javazada/src/variaveiseafins/variaveiseafins2.java
@@ -0,0 +1,66 @@
+package variaveiseafins;
+
+public class variaveiseafins2 {
+
+
+    /** Classes de Variáveis, Tipos de Dados e Operadores aritméticos
+     * 2 variáveis pra cada tipo de dado
+     */
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short p1 = 40000;
+
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005005000500005L;
+
+        // se não colocar o f no final o java acha que é double direto
+        //float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.84d;
+
+        // char é apenas 1 caractere
+        char c1 = 'W';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "pablosemhe4rt";
+        String st2 = "gdssvpp";
+        String st3 = "ag dgb se  se et t KNBKNBKNK %!#%@#%@^@#$";
+
+        // não usa STRING pra colocar número meu amor
+        // usar dado CORRETO pra DATAS -- Outro tipo de variável
+        //String dt1 = "14/07/1946";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+    }
+}
