62ab92b173d8f6dc4af46a2c7b9fcb0c8f4e346e
gdssvpp <pablonogueira16@hotmail.com>
2022-01-17 21:00:37 -0300

    inicio dos operadores

--- javazada/src/variaveiseafins/operadoresaritm.java
+++ javazada/src/variaveiseafins/operadoresaritm.java
@@ -2,5 +2,11 @@ package variaveiseafins;
 
 public class operadoresaritm {
 
+    /** tipos de operadores
+     * pós fixado: exp++ ou exp--
+     * prefixado: ++exp ou --exp
+     * aritmético: +, -, *, / e %
+     * atribuição: =, +=, -=, *=, /= e %=
+     */
 
 }
