f461fba60f7889f27be0bb5cd2dc1deef473eb8a
gdssvpp <pablonogueira16@hotmail.com>
2022-01-23 18:37:58 -0300

    Adição de exercícios

--- javazada/src/Exercicios/Ex6_Fatorial.java
+++ javazada/src/Exercicios/Ex6_Fatorial.java
@@ -1,4 +1,23 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Digite um número: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.print(fatorial + "! = ");
+        for(int i = fatorial; i >= 1; i = i - 1) {
+            multiplicacao = multiplicacao * i;
+        }
+
+        System.out.println(multiplicacao);
+
+    }
 }
